62c13592ad4b07a7ced35fe569ec4bd872ec2860
Rory Fitzpatrick <js08rgjf@foe-linux-01.leeds.ac.uk>
2017-01-17 22:12:43 +0000

    Uploaded CMIP5 user interface for AMMA-2050 project

---
+++ metric-GUI/filepaths.py
@@ -0,0 +1,15 @@
+
+
+def main(string):
+
+	if string == 'outp':
+		string = '/nfs/a266/earv061'
+	elif string == 'inp':
+		string = '/nfs/a266/data/CMIP5_Africa'
+	return(string)
+
+
+
+
+if "__name__" == "__main__":
+	main(string)
