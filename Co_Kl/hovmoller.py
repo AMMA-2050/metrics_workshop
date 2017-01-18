9470bd9c4b70e919f1c4b6382658771faf024352
cornkle <cornelia.klein@gmx.de>
2017-01-18 10:44:35 +0000

    add monthly SPI calculation file

---
+++ Co_Kl/hovmoller.py
@@ -0,0 +1,27 @@
+import iris
+import matplotlib.pyplot as plt
+
+
+
+def make_hovmoller(cube, x, y):
+
+pass
+
+
+
+
+
+
+
+
+def run(x, y):
+
+    path = "C:/Users/cornkle/data/CEH/AMMA/workshop2016/test_data/pr_day_HadGEM2-ES_west-africa_0.5x0.5_historical_r1i1p1_full.nc"
+    #cube = iris
+    pass
+
+
+
+
+if __name__ == '__main__':
+    run()
